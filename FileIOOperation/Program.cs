f0f7c3fe2c7ad4846254c96f263e7c55e6a3d2e7
Manoj Pawane <manojpawane@gmail.com>
2020-09-19 12:12:33 +0530

    File IO operation

---
+++ FileIOOperation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOperation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program.FileExists();
+            Program.FileCopy();
+            Program.ReadAllLines();
+            Program.ReadAllText();
+            Program.DeleteFile();
+            ReadThroughStreamReader readThroughStreamReader = new ReadThroughStreamReader();
+            readThroughStreamReader.WriteUsingStreamWriter();
+            readThroughStreamReader.ReadFromStreamReader();
+        }
+
+        public static void FileExists()
+        {
+            String path = @"E:\Example.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadKey();
+        }
+
+        public static void ReadAllLines()
+        {
+            String path = @"E:\Example.txt";
+
+            String[] lines;
+            lines = File.ReadAllLines(path);
+
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+
+            Console.ReadKey();
+        }
+
+        public static void ReadAllText()
+        {
+            String path = @"E:\Example.txt";
+
+            String lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+            Console.ReadKey();
+        }
+
+        public static void FileCopy()
+        {
+            String path = @"E:\Example.txt";
+
+            String copypath = @"E:\ExampleNew.txt";
+
+            File.Copy(path, copypath);
+
+            Console.ReadKey();
+        }
+
+        public static void DeleteFile()
+        {
+            String path = @"E:\Example.txt";
+
+            File.Delete(path);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileIOOperation/ReadThroughStreamReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOperation
+{
+    public class ReadThroughStreamReader
+    {
+        public void WriteUsingStreamWriter()
+        {
+            String path = @"E:\Example.txt";
+
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hello world - .Net is awesome");
+                sr.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+
+        public void ReadFromStreamReader()
+        {
+            String path = @"E:\Example.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
